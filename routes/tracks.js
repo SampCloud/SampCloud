034a7146053204e8956d88255ee7e9c702cc7339
David Bueno <davidbuenox8@gmail.com>
2021-04-26 19:28:31 +0200

    fixed

--- routes/tracks.js
+++ routes/tracks.js
@@ -3,10 +3,16 @@ const { loginCheck, isSinger, isProducer } = require("./middlewares");
 const User = require('../models/User');
 const { uploaderAudio, cloudinary } = require("../config/cloudinary");
 const Track = require('../models/Track');
+const streamifier = require('streamifier');
+
 
 
 router.get('/singerProfile/addTrack', (req, res, next) => {
   res.render('users/singer/addTrack');
+})
+
+router.get('/producerProfile/addTrack', (req, res, next) => {
+  res.render('users/producer/addTrack');
 })
 
 router.post('/singerProfile/addTrack', uploaderAudio.single('audio'), (req, res, next) => {
@@ -14,7 +20,11 @@ router.post('/singerProfile/addTrack', uploaderAudio.single('audio'), (req, res,
   const filePath = req.file.path;
   const fileName = req.file.originalname;
   const publicId = req.file.filename;
+  //const uploadLocation = __dirname + '/uploads/' + req.file.originalname;
   const buffer = req.file.buffer;
+  //fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(buffer)));
+
+
   console.log(req.file);
   Track.create({
     title: title,
@@ -27,6 +37,7 @@ router.post('/singerProfile/addTrack', uploaderAudio.single('audio'), (req, res,
     owner: req.session.user._id
   })
     .then(track => {
+      console.log(track)
       res.redirect('/singerProfile');
     })
     .catch(err => {
