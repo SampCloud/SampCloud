8a346ca2cbca4e0eacef594f357b673d19129776
Chiara Wilden <chiarawilden@gmail.com>
2021-04-28 01:00:03 +0200

    Final style edits day 2

--- public/js/profileModal.js
+++ public/js/profileModal.js
@@ -10,6 +10,10 @@ document.getElementById('button').addEventListener("click", function() {
 	document.querySelector('.bg-modal').style.display = "flex";
 });
 
+document.getElementById('button2').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
 document.querySelector('.close').addEventListener("click", function() {
 	document.querySelector('.bg-modal').style.display = "none";
 });
--- routes/tracks.js
+++ routes/tracks.js
@@ -4,6 +4,28 @@ const User = require('../models/User');
 const { uploaderAudio, cloudinary } = require("../config/cloudinary");
 const Track = require('../models/Track');
 const streamifier = require('streamifier');
+const fs = require('fs');
+
+router.get('/search', (req, res, next) => {
+  Track.find()
+    .then(tracks => {
+      res.render('search', { tracklist: tracks })
+    });
+})
+
+router.post('/search', (req, res, next) => {
+  console.log('query', req.body)
+  Track.find()
+    .then(tracks => {
+      const filteredTracks = tracks.filter(track => {
+        return track.title.toLowerCase().includes(req.body.q.toLowerCase())
+
+      })
+      res.render('search', { tracklist: filteredTracks })
+    })
+})
+
+
 
 
 
@@ -15,36 +37,50 @@ router.get('/producerProfile/addTrack', (req, res, next) => {
   res.render('users/producer/addTrack');
 })
 
-router.post('/singerProfile/addTrack', uploaderAudio.single('audio'), (req, res, next) => {
-  const { title, genre, description } = req.body;
-  const filePath = req.file.path;
-  const fileName = req.file.originalname;
-  const publicId = req.file.filename;
-  //const uploadLocation = __dirname + '/uploads/' + req.file.originalname;
-  const buffer = req.file.buffer;
-  //fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(buffer)));
 
 
-  console.log(req.file);
-  Track.create({
-    title: title,
-    genre: genre,
-    description: description,
-    filePath: filePath,
-    fileName: fileName,
-    publicId: publicId,
-    buffer: buffer,
-    owner: req.session.user._id
-  })
-    .then(track => {
-      console.log(track)
-      res.redirect('/singerProfile');
-    })
-    .catch(err => {
-      next(err);
-    })
+router.post('/singerProfile/addTrack', uploaderAudio.single('audio'), (req, res) => {
+  console.log('Got file:', req.file.originalname);
+  console.log('Extra form fields:', req.body);
+
+
+  cloudinary.uploader.upload_stream({ resource_type: "video" }, cloudinaryDone).end(req.file.buffer);
+
+  // After the upload is completed, this callback gets called
+  function cloudinaryDone(error, result) {
+    if (error) {
+      console.log("Error in cloudinary.uploader.upload_stream\n", error);
+      return;
+    } else {
+      const { title, genre, description } = req.body;
+      const fileName = req.file.originalname;
+      const publicId = req.file.filename;
+      console.log("Cloudinary audio info: ", result.audio);
+
+      console.log('Cloudinary url', result.url);
+      Track.create({
+        title: title,
+        genre: genre,
+        description: description,
+        imgPath: 'https://res.cloudinary.com/davidx8/image/upload/v1619543579/avatar-uploads/poolside-pack_qst27z.png',
+        filePath: result.url,
+        fileName: fileName,
+        publicId: publicId,
+        owner: req.session.user._id
+      })
+        .then(track => {
+          console.log(track)
+          res.redirect('/singerProfile');
+        })
+        .catch(err => {
+          next(err);
+        })
+    }
+
+
+
+  }
 });
-
 
 
 module.exports = router
