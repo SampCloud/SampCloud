b644ce1ea23a39b34a2b8e68d5dbd2d8f0d60e95
Chiara Wilden <chiarawilden@gmail.com>
2021-04-28 11:33:13 +0200

    Updated popup modals

--- public/js/profileModal.js
+++ public/js/profileModal.js
@@ -6,15 +6,41 @@ document.addEventListener(
   false
 );
 
-document.getElementById('button').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "flex";
+// document.getElementById('button').addEventListener("click", function() {
+// 	document.querySelector('.modal-bg').style.display = "flex";
+// });
+
+// document.querySelector('.close').addEventListener("click", function() {
+// 	document.querySelector('.modal-bg').style.display = "none";
+// });
+
+let modalButtons = [...document.querySelectorAll(".button")];
+modalButtons.forEach(function (button) {
+  button.onclick = function () {
+    let modal = button.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  }
 });
 
-document.getElementById('button2').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "flex";
+let closeButtons = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function (button) {
+  button.onclick = function () {
+    let modal = button.closest('.modal');
+    modal.style.display = "none";
+  }
 });
 
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "none";
-});
+window.onclick = function (event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+}
+
+
+
+
+
+// document.querySelector('.close').addEventListener("click", function() {
+// 	document.querySelector('.modal-bg').style.display = "none";
+// });
 
