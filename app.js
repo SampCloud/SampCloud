e292ab2fedec2473ec2b0ad89396ebbc7875e022
Chiara Wilden <chiarawilden@gmail.com>
2021-04-30 09:48:58 +0200

    Index updated final styling

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ const projectName = "artistsmatch";
 const capitalized = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
+app.locals.title = "SampCloud | An Idea";
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
